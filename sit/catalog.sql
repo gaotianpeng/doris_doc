527a49a8ab5407122f3a33114440d1eb12df8330
gaotianpeng <gtpsydm@gmail.com>
2024-03-12 17:56:38 +0800

    daily practise

--- sit/catalog.sql
+++ sit/catalog.sql
@@ -13,6 +13,14 @@ create catalog cirrofs properties (
 -- 查询表的列
 SHOW COLUMNS FROM table_name;
 
+-- Rename the catalog
+ALTER CATALOG catalog_name RENAME new_catalog_name;
+
+-- Modify / add properties for the catalog
+ALTER CATALOG catalog_name SET PROPERTIES ('key1' = 'value1' [, 'key' = 'value2']); 
+
+-- Modify comment for the catalog
+ALTER CATALOG catalog_name MODIFY COMMENT "new catalog comment";
 
 
 
